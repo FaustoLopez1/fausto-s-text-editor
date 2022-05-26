f3ea773992d523a1ae9b80b613e58faeb69808a6
Fausto Lopez <lopezfausto123@yahoo.com>
2022-05-26 01:01:04 -0700

    push to heroku fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,8 +1,9 @@
-import { openDB } from "idb";
+import { openDB, deleteDB } from "idb";
 
-const initdb = async () =>
+const initdb = async () => {
   openDB("jate", 1, {
     upgrade(db) {
+      console.log("upgrade");
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
@@ -11,6 +12,7 @@ const initdb = async () =>
       console.log("jate database created");
     },
   });
+};
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
@@ -18,18 +20,26 @@ export const putDb = async (content) => {
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ jate: content });
-  const result = await request;
-  console.error("putDb not implemented", result);
+  await request;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log("getDb");
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.getAll();
-  const result = await request;
-  console.error("getDb not implemented", result);
+  await request;
 };
 
+export const deletemyDB = async () => {
+  await deleteDB("jate", {
+    blocked() {
+      console.log("blocked");
+    },
+  });
+};
+console.log("preinit");
 initdb();
+//deletemyDB();
