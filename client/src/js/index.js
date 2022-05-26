de9f6fc5f7823a43cfdea49b8b3a08cdcd208bbd
Fausto Lopez <lopezfausto123@yahoo.com>
2022-05-25 22:08:17 -0700

    almost finished

---
+++ client/src/js/index.js
@@ -0,0 +1,29 @@
+import Editor from "./editor";
+import "./database";
+import "../css/style.css";
+
+const main = document.querySelector("#main");
+main.innerHTML = "";
+
+const loadSpinner = () => {
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
+
+const editor = new Editor();
+
+if (typeof editor === "undefined") {
+  loadSpinner();
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
